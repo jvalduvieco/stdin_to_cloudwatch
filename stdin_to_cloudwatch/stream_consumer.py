c6e2fca354165cce03e2f8089037e35b47235dbf
Joan Valduvieco <jvalduvieco@gmail.com>
2018-12-24 13:02:33 +0100

    missing file II...

---
+++ stdin_to_cloudwatch/stream_consumer.py
@@ -0,0 +1,16 @@
+import json
+
+from stdin_to_cloudwatch.publish_json_metrics import publish_metrics
+
+
+def is_json_metric(line):
+    return line[0] == '{' and line[-1] == '}'
+
+
+def eat_stream_and_publish_metrics_until_the_end(input_stream, output_function, input_adapter, cw_client, instance_id):
+    for line in input_stream:
+        adapted_line = input_adapter(line)
+        if is_json_metric(adapted_line):
+            publish_metrics(cw_client, json.loads(adapted_line), instance_id)
+        else:
+            output_function(line)
